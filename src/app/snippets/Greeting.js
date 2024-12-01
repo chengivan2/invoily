39e81730e25fc25a1811fe70ce2eb15d1d8d4c5b
chengivan <chengoleivan@gmail.com>
2024-12-01 23:03:30 +0300

    Testing 10

--- src/app/snippets/Greeting.js
+++ src/app/snippets/Greeting.js
@@ -3,7 +3,7 @@ import { createClient } from '../utils/supabase/server';
 export default async function Greeting() {
   const supabase = await createClient();
   const { data: user } = await supabase.from("users").select();
-  console.log(data)
+  console.log(user)
 
   return <pre>{JSON.stringify(user["name"], null, 2)}</pre>
 }
