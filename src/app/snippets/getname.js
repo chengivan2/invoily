0f3f2450c48352b0f1ad250d51ff3977eff7c78f
chengivan <chengoleivan@gmail.com>
2024-12-01 22:47:18 +0300

    Testing 6

--- src/app/snippets/getname.js
+++ src/app/snippets/getname.js
@@ -1,20 +1,25 @@
+" use client ";
 import { createBrowserClient } from "@supabase/ssr";
 
 export default async function Dashboard() {
-    // Create a single supabase client for interacting with your database
-    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-    const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey)
+  // Create a single supabase client for interacting with your database
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey);
 
-    const { data, error } = await supabase
-        .schema('public')
-        .from('users')
-        .select('name')
+  const { data, error } = await supabase
+    .schema("public")
+    .from("users")
+    .select("name");
 
   return (
     <div>
       <h1>Welcome to the Dashboard</h1>
-      {data ? <p>Hello, {data}!</p> : <p>You need to be logged in to view your data.</p>}
+      {data ? (
+        <p>Hello, {data}!</p>
+      ) : (
+        <p>You need to be logged in to view your data.</p>
+      )}
     </div>
-  )
+  );
 }
